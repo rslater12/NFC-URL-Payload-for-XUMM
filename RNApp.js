3f24294902d2dc8d45b2622f2f891dc238459986
rslater12 <slater.richard@hotmail.co.uk>
2020-03-24 10:07:59 +0000

    Update RNApp.js

--- RNApp.js
+++ RNApp.js
@@ -17,7 +17,8 @@ import {
 import NfcManager, { NfcTech, NfcAdapter, tech, Nfc,} from 'react-native-nfc-manager';
 import AwesomeAlert from 'react-native-awesome-alerts';
 
-//let text;
+let text;
+let url;
 
 class App extends React.Component {
     constructor(props){
@@ -32,6 +33,7 @@ class App extends React.Component {
      
     componentDidMount() {
         NfcManager.start()
+        Linking.addEventListener('url', this._handleOpenURL);
        
    
     }
@@ -43,6 +45,10 @@ class App extends React.Component {
     _cleanUp = () => {
         NfcManager.cancelTechnologyRequest().catch(() => 0);
     }
+    
+    _handleOpenURL(event) {
+    	  console.log(event.url);
+    	}
  
     readData = async () => {
  
@@ -97,13 +103,20 @@ class App extends React.Component {
         }
         try
         {
-        	var url = "'" + text + "'";
         	
-        	Linking.openURL(url);
-        	
-        }
+        	url = this.state.log;
+        
+        	Linking.canOpenURL(url)
+        	  .then((supported) => {
+        	    if (!supported) {
+        	      console.log("Can't handle url: " + url);
+        	    } else {
+        	      return Linking.openURL(url);
+        	    }
+        	  })
+    }
         catch (error) {
-            alert( "Error Loading URL")
+            alert( "Error Loading URL: " + url)
         }
    
 
@@ -112,7 +125,64 @@ class App extends React.Component {
  
    
  
+    onChangeText = (text) => {
+        this.setState({
+            text
+        })
+    }
     
+    writeData = async () => {
+        if (!this.state.text){
+            Alert.alert("Nothing to write");
+            return;
+        }
+        try {
+            let tech = Platform.OS === 'ios' ? NfcTech.MifareIOS : NfcTech.NfcA;
+            let resp = await NfcManager.requestTechnology(tech, {
+                alertMessage: 'Ready to do some custom Mifare cmd!'
+            });
+
+            let text = this.state.text;
+            let fullLength = text.length + 7;
+            let payloadLength = text.length + 3;
+
+            let cmd = Platform.OS === 'ios' ? NfcManager.sendMifareCommandIOS : NfcManager.transceive;
+
+            resp = await cmd([0xA2, 0x04, 0x03, fullLength, 0xD1, 0x01]); // 0x0C is the length of the entry with all the fluff (bytes + 7)
+            resp = await cmd([0xA2, 0x05, payloadLength, 0x54, 0x02, 0x65]); // 0x54 = T = Text block, 0x08 = length of string in bytes + 3
+
+            let currentPage = 6;
+            let currentPayload = [0xA2, currentPage, 0x6E];
+
+            for(let i=0; i<text.length; i++){
+                currentPayload.push(text.charCodeAt(i));
+                if (currentPayload.length == 6){
+                    resp = await cmd(currentPayload);
+                    currentPage += 1;
+                    currentPayload = [0xA2, currentPage];
+                }
+            }
+
+            // close the string and fill the current payload
+            currentPayload.push(254);
+            while(currentPayload.length < 6){
+                currentPayload.push(0);
+            }
+
+            resp = await cmd(currentPayload);
+
+            this.setState({
+                log: resp.toString()
+            })
+
+            this._cleanUp();
+        } catch (ex) {
+            this.setState({
+                log: ex.toString()
+            })
+            this._cleanUp();
+        }
+    } 
  
   
      
@@ -141,6 +211,20 @@ class App extends React.Component {
                 <Text>{this.state.log}</Text>
             </View>
 
+            <TextInput
+                              style={styles.textInput1}
+                               onChangeText={this.onChangeText}
+                              autoCompleteType="off"
+                               autoCapitalize="none"
+                               autoCorrect={false}
+                               placeholderTextColor="#888888"
+                               placeholder="Insert XRP Address to write" />
+
+                            	   <TouchableOpacity
+                             style={styles.buttonRead}
+                               onPress={this.writeData}>
+                             <Text style={styles.buttonText}>Write</Text>
+                           </TouchableOpacity>
                      
              
  
